f36ddde888ea5e05e7b9c8e8f7780e4d1697611d
Rachel Manzelli <manzelli@bu.edu>
2017-12-31 14:49:11 -0500

    Add smoother test file

--- test.py
+++ test.py
@@ -9,11 +9,9 @@ import tensorflow as tf
 def getNearestCentroid(input_pcap_f_name, centroids_f_name='centroids.obj'):
 	"""
 	Calculates euclidean distances of input pcap from each centroid and returns the closest centroid
-
 	@param input_pcap_f_name: input pcap file to be classified
 	@param centroids_f_name: name of pickled num_centroids x 128 np.array file
 	@return neartest_centroid: id number of nearest centroid
-
 	"""
 	# load the calculated centroids from k means
 	with open(centroids_f_name, 'rb') as fHandler:
@@ -56,14 +54,13 @@ if __name__ == "__main__":
 
 	# Live capture packet to check for malware
 	print("Live capturing packets...")
+
+	#LIVE CAPTURE
 	while True:
 		packets = sniff(count=5)
 		wrpcap('sniffsniff.pcap', packets)
 
-		# predict what cluster the pcap file belongs to
-		pred = getNearestCentroid("D:\\regin-malware\\00badda4-2f68-4cfb-9d1a-49bcc14c6d2b.pcap")
-		#pred = getNearestCentroid("D:\\normal_data\\normal1.pcap")
-
+		# predict cluster of captured packet
 		print("Predicting security of packets...")
 		pred = getNearestCentroid("./sniffsniff.pcap")
 
@@ -78,6 +75,23 @@ if __name__ == "__main__":
 
 		if prob['normal'] <= prob['mal']:
 			print("Malware found!")
-			sendSms()
+		    sendSms(prob['mal'])
 		else:
 			print("Packets are predicted to be normal traffic.")
+
+	# PREDICT MALWARE
+	# pred = getNearestCentroid("C:\\Users\\raman\\Documents\\EC441\\malware\\00badda4-2f68-4cfb-9d1a-49bcc14c6d2b.pcap")
+	# print("Predicted cluster is ", pred)
+		
+	# # get the malicious and normal probabilities
+	# prob = probs[pred]
+
+	# # send alert if probability of pcap file being malicious is more than that of normal
+	# print("Probability that the packet is normal traffic is ", prob['normal'])
+	# print("Probability that the packet is malicious traffic is ", prob['mal'])
+
+	# if prob['normal'] <= prob['mal']:
+	# 	print("Malware found!")
+	# 	sendSms(prob['mal'])
+	# else:
+	# 	print("Packets are predicted to be normal traffic.")
